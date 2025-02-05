a595d500dc23c1bf9d9298b80e683d07b47bee72
Gaurav <gauravshokanda2@gmail.com>
2025-02-06 00:06:25 +0530

    Support Custom Delimiters

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,6 +1,6 @@
 const add = require("./stringCalculator");
 
-test("handles new lines between numbers", () => {
-    expect(add("1\n2,3")).toBe(6);
+test("supports custom delimiter defined in format //;\n1;2", () => {
+    expect(add("//;\n1;2")).toBe(3);
   });
   

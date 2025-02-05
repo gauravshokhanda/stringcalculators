337af7449f6c865c84218c6e6b60f30af3472cc0
Gaurav <gauravshokanda2@gmail.com>
2025-02-06 00:06:00 +0530

    Support New Line (\n) as a Delimiter

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,6 +1,6 @@
 const add = require("./stringCalculator");
 
-test("returns sum of multiple comma-separated numbers", () => {
-    expect(add("1,2,3,4")).toBe(10);
+test("handles new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
   });
   
